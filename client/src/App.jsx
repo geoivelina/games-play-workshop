4f79585a5cfa902e3122c713023feed40c58e234
geoivelina <geoivelina@hotmail.com>
2023-11-15 13:44:45 +0200

    GameCatalog page Added
    Navigation links changed

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,17 @@
+import { Routes, Route } from "react-router-dom";
 import Header from "./components/Header/Header";
+import Home from "./components/Home/Home";
+import GameCatalog from "./components/GameCatalog/GameCatalog";
 
 function App() {
     return (
         <div id="box">
             <Header />
+
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/games" element={<GameCatalog />} />
+            </Routes>
         </div>
     );
 }
