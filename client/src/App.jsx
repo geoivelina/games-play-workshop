7c3f4fa1b3653310044724144f0593792a45321e
geoivelina <geoivelina@hotmail.com>
2023-11-15 13:47:40 +0200

    GameCreate page Added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,7 @@ import { Routes, Route } from "react-router-dom";
 import Header from "./components/Header/Header";
 import Home from "./components/Home/Home";
 import GameCatalog from "./components/GameCatalog/GameCatalog";
+import GameCreate from "./components/GameCreate/GameCreate";
 
 function App() {
     return (
@@ -11,6 +12,7 @@ function App() {
             <Routes>
                 <Route path="/" element={<Home />} />
                 <Route path="/games" element={<GameCatalog />} />
+                <Route path="/games/create" element={<GameCreate />} />
             </Routes>
         </div>
     );
